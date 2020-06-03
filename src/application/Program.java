3c1dd85aead7b8a990906a7d06783fcce0648ce2
Alexandro Pinto <alexandro.esp@gmail.com>
2020-06-03 07:10:28 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,7 +15,9 @@ public class Program {
 		
 		Seller seller = new Seller(2, "Jose", "jose@ze.com", new Date(), 3000.0, obj);
 		
-		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println(sellerDao);
 		
 	}
 
